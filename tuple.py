82e31e8f9fa49f3c03b9d079aeb99c53c960d5e7
Kaaviya <kaaviya@testpress.in>
2022-09-19 18:44:31 +0530

    Add tuple

---
+++ tuple.py
@@ -0,0 +1,12 @@
+values=(21,23,-45,56)
+print(values)
+print(values[3])
+print(values[0:])
+for i in values:
+    if i>0  :
+     print(i)
+print (values[3:1])
+c=len(values)-1
+while(c>=0):
+    print(values[c])
+    c=c-1
