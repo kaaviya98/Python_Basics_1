7331072445ec6ba50b1bbdb22bbb439c3149cd0a
Kaaviya <kaaviya@testpress.in>
2022-09-19 18:57:05 +0530

    Add String basic concepts

---
+++ String_basics.py
@@ -0,0 +1,18 @@
+text="Hi "
+text1="there!!"
+print(text+" "+text1)
+#multiple lines
+text3='''Hi,
+how's it going?'''
+print(text3)
+print("Kaaviya\'s textbook is the one that says,\"Pride and Prejudice\"")
+print(text[1])
+print(text1[-1])
+print(text1[0:4])
+print(text1[5])
+#new_text=text*3
+#print(new_text)
+#for i in text3:
+#    print(i)
+print("going" in text3)
+print("ingo" in text3)
