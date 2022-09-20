59eccbbe60b4d78b4044f4ea558f31dc6d37a2f0
Kaaviya <kaaviya@testpress.in>
2022-09-20 14:35:44 +0530

    add tuple

--- tuple.py
+++ tuple.py
@@ -10,3 +10,7 @@ c=len(values)-1
 while(c>=0):
     print(values[c])
     c=c-1
+quote = "Talk is cheap. Show me the code."
+print("1.", quote[3])
+print("2.", quote[-3])
+print("3.", quote.replace("code", "program"))
